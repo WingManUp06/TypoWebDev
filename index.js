7a809ac2ef2b029a845709bc38eb868813f1efbf
Dajuan Jones Jr <basketballwingman@gmail.com>
2022-12-30 23:39:14 -0500

    Almosted fixed with RWD
    Im almost fixed with RWD on the index.html, I need to fix the contact form though!

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 console.log("Js Is working")
 
-const navLinks = document.querySelector(".links");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-let isToggleTrue;
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
 
-document.getElementById("burgerBars").addEventListener("click", () => {
-    isToggleTrue = !isToggleTrue
-    console.log(isToggleTrue)
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active")
+    navMenu.classList.remove("active")
+}))
 
-
-
-    if(isToggleTrue){
-        navLinks.style.display = "block";
-    } else {
-        navLinks.style.display = "none";
-    }
-})
+console.log("woprking?");
